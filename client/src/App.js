81e352ec8e6f7b686d2c9cf765bd1fa907a9c52d
Muneer Hameed <mhameedhatim90@gmail.com>
2025-06-15 09:07:58 -0700

    Setup complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/pages/Homepage";
+import Shopepage from "./components/pages/Shopepage";
+import CreateEventpage from "./components/pages/CreateEventpage";
+import Signuppage from "./components/pages/Signuppage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<Shopepage />} />
+          <Route path="/create-event" element={<CreateEventpage />} />
+          <Route path="/signup" element={<Signuppage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/pages/Shopepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Shopepage() {
+  return (
+    <div>Shopepage</div>
+  )
+}
+
+export default Shopepage
